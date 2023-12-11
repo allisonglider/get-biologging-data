99ca6cc9f1f7522c139793afc0718d5b10fbba10
Allison Patterson <allisonglider@gmail.com>
2023-12-11 08:57:03 -0800

    update db access

--- R/connect_biologging_database.R
+++ R/connect_biologging_database.R
@@ -4,6 +4,10 @@ require(dplyr)
 require(arrow)
 require(config)
 
+Sys.setenv('AWS_ACCESS_KEY_ID' = config::get()$aws_key,
+           'AWS_SECRET_ACCESS_KEY' = config::get()$aws_secret,
+           'AWS_DEFAULT_REGION' = config::get()$aws_region)
+
 db_loc <- 'raw_data/biologging-db.duckdb'
 
 if (dir.exists('raw_data') == F) {dir.create('raw_data')}
@@ -12,7 +16,7 @@ aws.s3::save_object(object = "biologging-db.duckdb",
                     bucket = 's3://arcticecology-biologging',
                     file = db_loc, overwrite = T)
 
-con <- duckdb::dbConnect(duckdb::duckdb(), dbdir = db_loc, read_only = TRUE)
+con <- duckdb::dbConnect(duckdb::duckdb(), dbdir = db_loc, read_only = T)
 
 gps <- arrow::open_dataset('s3://arcticecology-biologging/gps')
 tdr <- arrow::open_dataset('s3://arcticecology-biologging/tdr')
